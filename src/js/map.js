0f592efc0381fbc0d1c37c867ac407c32984947e
Illia_Kudimov <illia.kudimov@gmail.com>
2021-03-12 11:32:15 +0200

    add fonts. icons. desck-top layout

--- src/js/map.js
+++ src/js/map.js
@@ -1,22 +1,22 @@
-// // Initialize and add the map
-// function initMap() {
-//     // The location of Uluru
-//     const uluru = { lat: -25.344, lng: 131.036 };
-//     // The map, centered at Uluru
-//     const map = new google.maps.Map(
-//       document.getElementById("map"),
-//       {
-//         zoom: 4,
-//         center: uluru,
-//       }
-//     );
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(
+      document.getElementById("map"),
+      {
+        zoom: 4,
+        center: uluru,
+      }
+    );
   
-//     // The marker, positioned at Uluru
-//     const marker = new google.maps.Marker({
-//       position: uluru,
-//       map: map,
-//     });
-//   }
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
 
 
 // https://developers.google.com/maps   AIzaSyBQ8u4Tg1iPaSqyvtvq1BhQiI16ARdJi8M
